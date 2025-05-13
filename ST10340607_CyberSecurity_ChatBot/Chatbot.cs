c55dd3e59f9649f1e859ad2730834bc86066fb1c
Justin Anderssen <ST10340607@imconnect.edu.za>
2025-05-13 12:15:38 +0200

    I updated the chatbot class

--- ST10340607_CyberSecurity_ChatBot/Chatbot.cs
+++ ST10340607_CyberSecurity_ChatBot/Chatbot.cs
@@ -9,7 +9,9 @@ namespace ST10340607_CyberSecurity_ChatBot
     public abstract class Chatbot// creation of the abstract class 
     {
         public abstract void cybersecurityQuestions();// abstract method that is overidden
-       
+        public virtual void userInterest(String topic) { } // used for the user interest for the method in the other class Cybersecuritychatbot  
+        public virtual string detectEmotion(string input) => "neutral"; // method used in the Cybersecuritychatbot class to habdle the emotion
+
 
     }
 }
